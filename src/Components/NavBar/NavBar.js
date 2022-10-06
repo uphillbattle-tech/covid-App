7df3290d27b949a76fb4a1f73ea0e6f5b579d4ae
mydocker11 <jacobrhlc.us@gmail.com>
2022-10-05 20:34:28 -0400

    new code19

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,8 +3,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Home from '../Navigation/Home';
-import CovidCases from '../Navigation/Covid-Reslut/Covid-Cases-Deaths';
-import HospitalBeds from '../USA-Hospital-Beds/USA-Hospital-Beds-result';
+import CovidCases from '../Navigation/Covid-Results/Covid-Cases-Deaths';
+import HospitalBeds from '../USA-Hospital-Beds/USA-Hospital-Beds-Results';
 
 
 import {
@@ -27,7 +27,7 @@ const NavBar = () => {
             <NavDropdown title="Covid Result" id="basic-nav-dropdown">
                 <Nav.Item as={Link} to="/covid-deaths">Covid-Cases-Deaths</Nav.Item>
                 <NavDropdown.Divider />
-                <Nav.Item as={Link} to="/hospital-beds">USA Hospital Beds Reslut</Nav.Item>
+                <Nav.Item as={Link} to="/hospital-beds">USA Hospital Beds Results</Nav.Item>
             </NavDropdown>
           </Nav>
         </Container>
